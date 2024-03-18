a5053e700505f6d893b100ccb0c6545b64289ec2
Vasil Voloshin <voloshin017@gmail.com>
2024-03-18 20:05:55 +0200

    init axios

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,44 @@
 <script setup>
-import { onBeforeMount } from "vue";
+import { onMounted, computed, ref } from "vue";
+import axios from "axios";
 
-onBeforeMount(() => {
-  console.log("test");
+const data = ref({});
+const list = ref(0);
+
+onMounted(() => {
+  // console.log(dayjs("2010-04-01").quarter(), dayjs("2010-04-01").quarter(2));
+
+  axios
+    .get("https://api.monobank.ua/personal/client-info", {
+      headers: {
+        "X-Token": "uXEb3eNHThCxm6ajZ1LlV1QzMucAg5Pu8nyWRsipx0cY",
+      },
+    })
+    .then((res) => (data.value = res.data.accounts.filter((i) => i.type === "fop")));
+
+  axios
+    .get("https://api.monobank.ua/personal/statement/_3xe8hv_38-86EWSbvZFyA/1709244000", {
+      headers: {
+        "X-Token": "uXEb3eNHThCxm6ajZ1LlV1QzMucAg5Pu8nyWRsipx0cY",
+      },
+    })
+    .then((res) => (list.value = res.data.filter((i) => i.invoiceId)));
 });
 </script>
 
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div>
+      <h1>{{ data[0]?.balance }}</h1>
+    </div>
+    <br />
+
+    <div>
+      <h2>{{ list[0]?.amount }}</h2>
+    </div>
+    <div>
+      <h4>{{ list[0]?.description }}</h4>
+    </div>
   </div>
 </template>
 
