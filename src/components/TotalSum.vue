bcae9acb364d89106203df5c357535346256624f
Vasil Voloshin <voloshin017@gmail.com>
2024-04-10 18:50:42 +0300

    added favicon

--- src/components/TotalSum.vue
+++ src/components/TotalSum.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 
 <template>
   <div class="bg-[#5B606E] shadow-gray-300 rounded-xl flex flex-col text-center mt-5 text-white p-5">
-    <div>Загальна сума податку</div>
+    <div class="text-sm">Загальна сума податку</div>
     <div class="text-3xl">{{ formatNumber(props.data) }} грн</div>
   </div>
 </template>
